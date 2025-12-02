72f9b2f5302b0381a59f680a47112bd85cf96663
hajimohamadiparham-tech <hajimohamadiparham@gmail.com>
2025-12-02 20:03:45 +0330

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,13 +1,12 @@
 
-async function login(){
-    let u=document.getElementById('user').value.trim();
-    let p=document.getElementById('pass').value.trim();
-    let res=await fetch('data/users.json');
-    let data=await res.json();
-    if(data[u] && data[u].password===p){
-        localStorage.setItem('phm_user',u);
-        window.location='pages/welcome.html';
+function login() {
+    let user = document.getElementById("user").value;
+    let pass = document.getElementById("pass").value;
+
+    // یوزر و پسوردی که می‌خوای
+    if (user === "admin" && pass === "1234") {
+        window.location.href = "pages/panel.html";
     } else {
-        alert('نام کاربری یا رمز اشتباه است');
+        alert("نام کاربری یا رمز اشتباه است");
     }
 }
